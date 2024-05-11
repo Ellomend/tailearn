14d9261ff5583738b8dac1af658b2bce056feed8
Ellomend <mrsotona@gmail.com>
2024-05-11 17:51:59 +0300

    installed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ const asdf = 'fsda'
 </script>
 
 <template>
-  <main>
-    <p>asdf {{ asdf }}</p>
+  <main class="bg-blue-500 text-white text-center p-4 rounded-lg shadow-lg">
+    <p class="text-lg ">asdf {{ asdf }}</p>
   </main>
 </template>
