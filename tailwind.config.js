b9e67ba09fd43a780c94fb583f4120cca98dd770
Ellomend <mrsotona@gmail.com>
2024-05-11 22:56:45 +0300

    fix tailwind extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      extend: {
-        colors: {
-          'primary': '#3490dc',
-          'secondary': '#ffed4a',
-          'danger': '#e3342f',
-          'brand-blue': '#007ace',
-          'brand-red': '#ff4136',
-        },
-      },
-    },
+      colors: {
+        primary: '#3490dc',
+        secondary: '#ffed4a',
+        danger: '#e3342f',
+        'brand-blue': '#007ace',
+        'brand-red': '#ff4136'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
