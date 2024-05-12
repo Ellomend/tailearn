ff50d9269b4813825194fd564839bd99f5244865
Ellomend <mrsotona@gmail.com>
2024-05-12 13:20:25 +0300

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import SidebarComponent from './components/SidebarComponent/SidebarComponent.vue';
 </script>
 
 <template>
+  <SidebarComponent />
   <RouterView />
 </template>
---
+++ src/components/SidebarComponent/SidebarComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <aside class="w-64 h-full bg-gray-200 p-4">
+    <ul>
+      <li class="font-bold mb-4">Categories</li>
+      <li v-for="category in categories" :key="category.id" class="mb-2 px-2 py-1 hover:bg-gray-300 rounded">
+        {{ category.name }}
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script setup>
+
+// make up some ecommerce categories
+const categories = [
+  { id: 1, name: 'Fruits and Vegetables' },
+  { id: 2, name: 'Meat and Fish' },
+  { id: 3, name: 'Dairy and Eggs' },
+  { id: 4, name: 'Bakery and Pastry' },
+  { id: 5, name: 'Drinks' },
+]
+</script>
